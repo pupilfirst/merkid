0817a9eb1d8fb5cf3bddb1a540e64831a08f6df4
Jasim A Basheer <jasim.ab@gmail.com>
2020-11-26 23:25:51 +0530

    fill form

--- app/controllers/student_application_forms_controller.rb
+++ app/controllers/student_application_forms_controller.rb
@@ -1,7 +1,7 @@
 class StudentApplicationFormsController < ApplicationController
   before_action :fetch_active_student
   before_action :ensure_active_student
-
+  before_action :allow_only_email_verified_students
 
   def new
     @email = @student.email
@@ -9,15 +9,17 @@ class StudentApplicationFormsController < ApplicationController
 
   def create
     p = params[:student]
-    @student.update_attributes!(first_name: p[:first_name])
+    @student.submit_application_form!(first_name: p[:first_name])
     redirect_to students_path
   end
 
   private
+
   def allow_only_email_verified_students
-    # if application already filled, can't update.
+    # if application already filled, can't update. can only do
+    # the subsequent steps
     unless @student.status_email_verified?
-      flash[:info] = "Please login to visit that page"
+      flash[:info] = "Application is already filled"
       redirect_to root_path
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,31 @@
 class User < ApplicationRecord
+  EMAIL_UNVERIFIED = "email_unverified"
+  EMAIL_VERIFIED = "email_verified"
+  APPLICATION_FORM_SUBMITTED = "application_form_submitted"
+
   def self.create_student!(email)
-    create!(email: email.downcase, status: "email_unverified")
+    create!(email: email.downcase, status: EMAIL_UNVERIFIED)
   end
 
   def status_email_unverified?
-    status == "email_unverified"
+    status == EMAIL_UNVERIFIED
   end
 
   def status_email_verified?
-    status == "email_verified"
+    status == EMAIL_VERIFIED
   end
 
   def status_application_form_submitted?
-    status == "application_form_submitted"
+    status == APPLICATION_FORM_SUBMITTED
   end
 
+  # workflow operations
   def mark_verified!
-    update_attributes!(status: "email_verified") if status_email_unverified?
+    update_attributes!(status: EMAIL_VERIFIED) if status_email_unverified?
+  end
+
+  def submit_application_form!(h)
+    update_attributes!(status: APPLICATION_FORM_SUBMITTED, first_name: h[:first_name])
   end
 
   def display_name
