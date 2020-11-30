136674a29debd0486015f265de315cdc78d7a60f
Jasim A Basheer <jasim.ab@gmail.com>
2020-12-01 00:05:59 +0530

    remove custom sprockets

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,52 +24,6 @@ set :migration_role, :app
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 
-# use the custom google cloud ssh access key which terraform configures servers with
-# set :ssh_options, {:keys => ["#{ENV['HOME']}/.ssh/gcloud_id_rsa"]}
-# we're now using DigitalOcean and so standard ssh keys (jasim, sherin) are copied.
-
-def clear_task(t)
-  Rake::Task[t].clear if Rake::Task.task_defined?(t)
-end
-
-clear_task 'deploy:assets:precompile'
-
-# https://sourcey.com/articles/precompiling-assets-for-large-rails-deployments-with-capistrano
-# http://archive.is/bwykD
-namespace :deploy do
-  namespace :assets do
-    desc "Precompile assets locally and then rsync to deploy server"
-    task :precompile do
-      assets = fetch(:assets_prefix)
-      packs = fetch(:packs_dir)
-      run_locally do
-        execute "RAILS_ENV=production bundle exec rake assets:precompile"
-      end
-      on roles(:app) do |host|
-        hostname = host.hostname
-        user = host.user
-        run_locally do
-          cmd_string = "rsync -av ./public/#{assets}/ #{user}@#{hostname}:#{release_path}/public/#{assets}/"
-          execute cmd_string
-
-          cmd_string = "rsync -av ./public/packs/ #{user}@#{hostname}:#{release_path}/public/packs/"
-          execute cmd_string
-        end
-
-        within(release_path) do
-          yarn_install = "NODE_ENV=production yarn install --no-progress --no-audit --frozen-lockfile --production --no-optional"
-          execute "cd #{release_path} && #{yarn_install}"
-        end
-      end
-
-      run_locally do
-        execute "rm -rf public/#{assets}"
-        execute "rm -rf public/packs"
-      end
-    end
-  end
-end
-
 namespace :deploy do
   # restart the node server as well
   after :finished, 'pm2:restart'
