f8ac27bdc2cf198fa4d9f7442c4ebb2c914b9725
Bodhish Thomas <bodhish@gmail.com>
2020-12-02 15:06:39 +0530

    Update application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,6 +3,6 @@ class ApplicationMailer < ActionMailer::Base
   # Postmark::ApiInputError (The 'From' address you supplied (fullstack@pupilfirst.org)
   # is not a Sender Signature on your account. Please add and confirm this address in order
   # to be able to use it in the 'From' field of your messages.)
-  default from: 'hello@protoship.io'
+  default from: 'fullstack@pupilfirst.org'
   layout 'mailer'
 end
