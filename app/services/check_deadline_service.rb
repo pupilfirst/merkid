287dc2565b915563744196b4157a638bb0ab0460
Bodhish Thomas <bodhish@gmail.com>
2021-01-05 22:30:38 +0530

    Consider timezone while computing the deadline

--- app/services/check_deadline_service.rb
+++ app/services/check_deadline_service.rb
@@ -1,6 +1,6 @@
 class CheckDeadlineService
   def disabled
-    deadline = DateTime.parse('2021-01-05').end_of_day
+    deadline = DateTime.parse('2021-01-05').in_time_zone('Asia/Kolkata').end_of_day
     Time.now.in_time_zone('Asia/Kolkata') > deadline
   end
 end
