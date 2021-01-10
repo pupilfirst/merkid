6473b8246b399f99118b98cd3c827a069b07d647
Jasim A Basheer <jasim.ab@gmail.com>
2021-01-10 12:26:34 +0530

    Large CSV download: use SQL directly to avoid instantiating large no of AR objects

--- app/services/students_data_export.rb
+++ app/services/students_data_export.rb
@@ -4,9 +4,9 @@ class StudentsDataExport
   def begin_export!
     filename = "/tmp/students-#{rand(1000)}"
     CSV.open(filename + '.csv', 'wb') do |csv|
-      csv << User.attribute_names
-      User.all.each do |user|
-        csv << user.attributes.values
+      csv << User.column_names
+      User.connection.select_all("select * from users").each do |row|
+        csv << row.values
       end
     end
 
